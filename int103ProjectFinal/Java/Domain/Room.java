02f6d8858a1a6f133a4e96dcaef87322750e9e3a
SMarKleZ <poramet.khan@kmutt.ac.th>
2024-06-05 00:03:50 +0700

    OhmAv

--- int103ProjectFinal/Java/Domain/Room.java
+++ int103ProjectFinal/Java/Domain/Room.java
@@ -3,5 +3,9 @@ package Domain;
 public class Room {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Mew sakhon");
+        System.out.println("Ohm.JavaProject");
+        System.out.println("");
     }
+
 }
