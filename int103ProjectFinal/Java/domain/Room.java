c54f14766e15145277eca067207dfacae2776214
KittiphopOhm <kittiphop.sang@mail.kmutt.ac.th>
2024-06-06 02:56:37 +0700

    Debug

--- int103ProjectFinal/Java/domain/Room.java
+++ int103ProjectFinal/Java/domain/Room.java
@@ -1,11 +1,89 @@
-package Domain;
+package domain;
 
-public class Room {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Mew sakhon");
-        System.out.println("Ohm.JavaProject");
-        System.out.println("OhmRukHee");
+import exception.InvalidRoomFormatException;
+
+import java.io.Serializable;
+
+public class Room implements Comparable<Room>, Serializable {
+    private final String roomNumber;
+    private String type;
+    private String capacity;
+    private String amenities;
+    private boolean available;
+    private double price;
+    public Room(String roomNumber, String type, String capacity, String amenities, double price) {
+        if (roomNumber == null || roomNumber.isBlank() || type == null || type.isBlank() || capacity == null
+                || capacity.isBlank() || amenities == null || amenities.isBlank() || price < 0.0)  throw new InvalidRoomFormatException();
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.capacity = capacity;
+        this.amenities = amenities;
+        this.price = price;
+        this.available = true;
     }
 
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        if (type == null || type.isBlank()) throw new InvalidRoomFormatException();
+        this.type = type;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        if (capacity == null || capacity.isBlank()) throw new InvalidRoomFormatException();
+        this.capacity = capacity;
+    }
+
+    public String getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(String amenities) {
+        if (amenities == null || amenities.isBlank()) throw new InvalidRoomFormatException();
+        this.amenities = amenities;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void reAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void rePrice(double price) {
+        if (price < 0.0) throw new InvalidRoomFormatException();
+        this.price = price;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Room: (RoomNumber:%s, Type:%s, Capacity:%s, Amenities:%s, Price:%.2f)", roomNumber, type, capacity, amenities, price);
+    }
+
+    @Override
+    public int compareTo(Room room) {return roomNumber.compareTo(room.roomNumber);}
 }
+
