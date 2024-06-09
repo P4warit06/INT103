ee9b411a9da6fc18b5a427735c7bde3622388f87
Teerapat Sangman <142310412+teerapatwee@users.noreply.github.com>
2024-06-09 13:42:53 +0700

    test

--- int103ProjectFinal/Java/repository/RoomRepository.java
+++ int103ProjectFinal/Java/repository/RoomRepository.java
@@ -9,7 +9,7 @@ public interface RoomRepository {
     Room createRoom(String type,String capacity, String amenities , double price);
     Room retrieveRoom(String roomNumber);
     Map<String, Room> getAllRoom();
-    boolean updateRoom(Room room);
-    boolean deleteRoom(Room room);
+    boolean updateRoom(Room room,String id);
+    boolean deleteRoom(Room room,String id);
     Stream<Room> stream();
 }
