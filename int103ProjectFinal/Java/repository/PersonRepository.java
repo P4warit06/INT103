fb5eaca080353bd2f6cc1451b3fcb1e3580d923a
KittiphopOhm <kittiphop.sang@mail.kmutt.ac.th>
2024-06-09 14:52:19 +0700

    Fix

--- int103ProjectFinal/Java/repository/PersonRepository.java
+++ int103ProjectFinal/Java/repository/PersonRepository.java
@@ -9,8 +9,8 @@ public interface PersonRepository {
     Person createPerson(String name, String email, String password) ;
     Person retrievePerson(String id);
     Person loginPerson(String email, String password);
-    boolean updatePerson(Person person,String id);
-    boolean deletePerson (Person person,String id) ;
+    boolean updatePerson(Person person);
+    boolean deletePerson (Person person) ;
     Stream<Person> stream();
 }
 
